8cdfd7155947f294231c9c97ec722d86e5ba89d3
Pedro José <pedrojosepaiva@outlook.com>
2020-03-18 17:47:41 -0300

    Calculator in react

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MenuBar.css'
+
+
+export default props => {
+
+    return (
+    <div className="menu-bar">
+        <button className="expand" ></button>
+    </div>
+    )
+}
